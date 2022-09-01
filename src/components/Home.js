17ae9d4dcbefb25eab6e70e36ebcce6801e0a7aa
vipinchandra597 <108525735+vipinchandra597@users.noreply.github.com>
2022-09-01 18:38:56 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+let Home = () => {
+    return (
+        <React.Fragment>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col text-center mt-5">
+                        <img src="https://hybridheroes.de/blog/2021-01-08-redux-toolkit-react-native/redux-toolkit-800.jpg" alt=""/>
+                        <p className="h3 display-3">Project with redux toolkit</p>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+};
+export default Home;
